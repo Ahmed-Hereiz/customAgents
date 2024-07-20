ce380dd2b74ec3c0e62408d51bf117c007161b16
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-20 19:34:54 +0300

    first working version of customAgents

---
+++ customAgnets/agent_prompt/simple_prompt.py
@@ -0,0 +1,9 @@
+from modules.agent_prompt import BasePrompt
+
+
+class SimplePrompt(BasePrompt):
+    def __init__(self, prompt_string: str = ""):
+
+        super().__init__(prompt_string)
+
+        
---
+++ customAgnets/agent_routers/toolexec_router.py
@@ -0,0 +1,16 @@
+from modules.agent_routers import BaseRouter
+from modules.agent_tools import BaseTool
+
+class ToolExecRouter(BaseRouter):
+    def __init__(self, tool: BaseTool, exec_after: float = 0):
+        super().__init__(exec_after)
+
+        self.tool = tool
+
+    def exec_router(self, *params):
+
+        return self.tool.execute_func(*params)
+
+
+
+    
