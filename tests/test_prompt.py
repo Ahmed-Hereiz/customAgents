4eb41bad095d5555a9a0403829c30f046bf588f2
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-21 13:08:56 +0300

    add print statement to test prompt

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -3,3 +3,5 @@ from modules.agent_prompt import ReActPrompt, PlaceHoldersPrompt, SimplePrompt
 assert type(ReActPrompt(question=".").prompt) == str
 assert type(PlaceHoldersPrompt().prompt) == str
 assert type(SimplePrompt().prompt) == str
+
+print("all assertions bassed")
