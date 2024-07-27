5f365fb77578f30e992234c5832293801367a618
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-07-27 20:43:04 +0300

    add routers param to base_env

--- customAgents/agent_env/base_env.py
+++ customAgents/agent_env/base_env.py
@@ -1,29 +1,29 @@
-from typing import List, Union, Any
+from typing import List, Any
 from customAgents.agent_runtime import BaseRuntime
 from customAgents.agent_routers import BaseRouter
 
-AgentType = Union[BaseRuntime, BaseRouter]
-
 
 class BaseEnv:
-    def __init__(self, env_runs: List[AgentType]):
+    def __init__(self, agents: List[BaseRuntime], routers: List[BaseRouter]):
 
-        self.env_runs = env_runs
+        self.agents = agents
+        self.routers = routers
 
     def run(self):
         """modify this in every class you create that inherit from BaseEnv"""
-        for env_run in self.env_runs:
-            print(f"agent in the agents list : {env_run}")
+        for agent in self.agents:
+            print(f"agent in the agents list : {agent}")
 
         raise NotImplementedError(f"Need to implement the run method first to specify how the env will work")
         
 
     def __str__(self) -> str:
-        return f"agents or routers in this env is {self.env_run}"
+        return f"agents in this env is {self.agents}, routers in this env is {self.routers}"
     
     def __add__(self, other) -> Any:
-        self.env_runs = self.env_runs + other.env_runs
+        self.agents = self.agents + other.agents
+        self.routers = self.routers + other.routers
 
     def __repr__(self) -> str:
-        return f"agents or routers in this env is {self.env_runs}"
+        return f"agents in this env is {self.agents}, routers in this env is {self.routers}"
     
