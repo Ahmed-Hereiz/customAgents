2d9b4705f7d994c8f51a0a0e16017f7e75793117
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-08-03 15:22:30 +0300

    add choosetools to the __init__ files

--- customAgents/agent_prompt/__init__.py
+++ customAgents/agent_prompt/__init__.py
@@ -2,10 +2,13 @@ from .base_prompt import BasePrompt
 from .simple_prompt import SimplePrompt
 from .placeholder_prompt import PlaceHoldersPrompt
 from .react_prompt import ReActPrompt
+from .choosetools_prompt import ChooseToolsPrompt
+
 
 __all__ = [
     'BasePrompt',
     'SimplePrompt',
     'PlaceHoldersPrompt',
-    'ReActPrompt'
+    'ReActPrompt',
+    'ChooseToolsPrompt'
 ]
