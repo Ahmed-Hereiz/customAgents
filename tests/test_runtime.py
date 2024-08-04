0856e933d2e32d0b20af4987bcf7ce91e7938cd8
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-08-04 15:35:37 +0300

    test choosetools

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -1,6 +1,6 @@
-from customAgents.agent_runtime import HumanLoopRuntime
+from customAgents.agent_runtime import HumanLoopRuntime, ChooseToolRuntime
 from customAgents.agent_llm import SimpleStreamLLM
-from customAgents.agent_prompt import SimplePrompt
+from customAgents.agent_prompt import SimplePrompt, ChooseToolsPrompt
 from common.utils import load_config, parse_safety_settings
 
 from utils import add_root_to_path
@@ -11,8 +11,8 @@ config = load_config(f"{root_path}/config/llm.json")
 safety_settings = parse_safety_settings(config['safety_settings'])
 
 
-stream_llm = SimpleStreamLLM(api_key=config['api_key'],model=config['model'],temperature=0.7)
-prompt = SimplePrompt("tell me 10 linux commands ")
-agent = HumanLoopRuntime(llm=stream_llm,prompt=prompt)
+# stream_llm = SimpleStreamLLM(api_key=config['api_key'],model=config['model'],temperature=0.7)
+# prompt = SimplePrompt("tell me 10 linux commands ")
+# agent = HumanLoopRuntime(llm=stream_llm,prompt=prompt)
 
-agent.loop()
+# agent.loop()
