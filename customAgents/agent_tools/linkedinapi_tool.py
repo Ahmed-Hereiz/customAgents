9f63267feb59b2db256f10cc6e744234a8e2b7da
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-08-15 19:57:41 +0300

    add the exec function method

--- customAgents/agent_tools/linkedinapi_tool.py
+++ customAgents/agent_tools/linkedinapi_tool.py
@@ -1,5 +1,11 @@
+from typing import Any
 from customAgents.agent_tools import BaseTool
 
 class LinkedINApiTool(BaseTool):
     def __init__(self, description: str, tool_name: str = None):
         super().__init__(description, tool_name)
+
+    def execute_func(self, *params: Any) -> Any:
+        """Needs to be customized !!"""
+
+        return super().execute_func(*params)
