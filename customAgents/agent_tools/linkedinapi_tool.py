080a439ea94b7b174419160cdd3d171cd0eeae65
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-08-18 15:25:59 +0300

    add linkedin_api_key param to tool class

--- customAgents/agent_tools/linkedinapi_tool.py
+++ customAgents/agent_tools/linkedinapi_tool.py
@@ -2,8 +2,10 @@ from typing import Any
 from customAgents.agent_tools import BaseTool
 
 class LinkedINApiTool(BaseTool):
-    def __init__(self, description: str, tool_name: str = None):
+    def __init__(self, linkedin_api_key: str, description: str, tool_name: str = None):
         super().__init__(description, tool_name)
+
+        self.linkedin_api_key = linkedin_api_key
 
     def execute_func(self, *params: Any) -> Any:
         """Needs to be customized !!"""
