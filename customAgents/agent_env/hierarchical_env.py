574f8bddcfe50340630ddae8b38cbe5e87664cbf
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-08-22 21:16:35 +0300

    start HierarchialEnv implement

--- customAgents/agent_env/hierarchical_env.py
+++ customAgents/agent_env/hierarchical_env.py
@@ -1,10 +1,22 @@
-from typing import List
+from typing import List, Union
 from customAgents.agent_env import BaseEnv
 from customAgents.agent_routers import BaseRouter
 from customAgents.agent_runtime import BaseRuntime
 
 
 class HierarchialEnv(BaseEnv):
-    def __init__(self, agents: List[BaseRuntime], routers: List[BaseRouter] = None):
-        super().__init__(agents, routers)
+    def __init__(self, env_items: Union[List[BaseRuntime], List[BaseRouter]]):
         
+        self.env_items = env_items
+
+        super().__init__(agents=None, routers=None)
+
+    def run(self):
+
+        for item in self.env_items:
+            if type(item) is BaseRuntime:
+                item.loop()
+            elif type(item) is BaseRouter:
+                item.exec_router()
+            elif type(item) is list:
+                pass
