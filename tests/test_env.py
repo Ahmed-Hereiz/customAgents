04e05717090700f2eb1b2924f5b2dedf6631d479
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-08-27 19:46:17 +0300

    test env script

--- tests/test_env.py
+++ tests/test_env.py
@@ -10,7 +10,9 @@ safety_settings = parse_safety_settings(config['safety_settings'])
 
 query_prompt_string = """
 You are search Assistant llm, your task is to help user where you reformat his query 
+
+user_query : {query}
+output : 
 """
 
 query_llm = SimpleStreamLLM(api_key=config['api_key'],model='gemini-1.5-flash',temperature=0.7,safety_settings=safety_settings)
-query_prompt = PlaceHoldersPrompt()
