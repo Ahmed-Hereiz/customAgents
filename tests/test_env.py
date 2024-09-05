93d2346fef0cb70ad3ea01d907e7660b173b2b40
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-09-05 23:12:34 +0300

    full sequential env test

--- tests/test_env.py
+++ tests/test_env.py
@@ -14,6 +14,7 @@ safety_settings = parse_safety_settings(config['safety_settings'])
 query_prompt_string = """
 You are search Assistant llm, your task is to help user where you reformat his query, you have to ouput only one sentence to the user.
 so that your sentence will go for other code that will execute this directly.
+NOTE THAT YOU JUST OUTPUT THE SINGLE SHORT QUERY THAT WILL MAKE BETTER SEARCH WITHOUT ANY FURTHER EXPLAINATION
 
 example :  
 user_query : I want to learn python while iam beginner and I have no experience in it.
@@ -44,11 +45,9 @@ text_output = tool_exec.exec_router(output_query)
 summary_llm = SimpleStreamLLM(api_key=config['api_key'],model='gemini-1.5-flash',temperature=0.7,safety_settings=safety_settings)
 summary_prompt = PlaceHoldersPrompt(placeholders={"{text}":text_output},prompt_string=summarize_prompt_string)
 summary_agent = SimpleRuntime(llm=summary_llm,prompt=summary_prompt)
-# final_summary = summary_agent.loop()
-
+final_summary = summary_agent.loop()
 
 print(">"*100)
 print("Equivlent with the env : \n\n")
 env = SequentialEnv(env_items=[query_agent,tool_exec,summary_agent])
 print(env.run(initial_input=query))
-print(type(env))
