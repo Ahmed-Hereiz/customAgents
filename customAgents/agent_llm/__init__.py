e4c3a8d5c23fe8a648bf303cbef35a62c5e6c22a
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-09-07 11:41:45 +0300

    add SimpleMultimodal class

--- customAgents/agent_llm/__init__.py
+++ customAgents/agent_llm/__init__.py
@@ -1,10 +1,15 @@
 
 from .base_llm import BaseLLM
+from .base_multimodal import BaseMultiModal
 from .simple_llm import SimpleLLM, SimpleInvokeLLM, SimpleStreamLLM
+from .simple_multimodal import SimpleMultiModal
+
 
 __all__ = [
     'BaseLLM',
     'SimpleLLM',
     'SimpleInvokeLLM',
-    'SimpleStreamLLM'
+    'SimpleStreamLLM',
+    'BaseMultiModal',
+    'SimpleMultiModal'
 ]
--- customAgents/agent_llm/base_llm.py
+++ customAgents/agent_llm/base_llm.py
@@ -1,12 +1,9 @@
 from typing import Any
-from customAgents.agent_llm.type_utils import agent_llm_type, agent_multimodal_type
+from customAgents.agent_llm.type_utils import agent_llm_type
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-from PIL import Image
-import google.generativeai as genai
-
 
 
 @agent_llm_type
@@ -198,57 +195,3 @@ class BaseLLM:
         return self._chain
     
     
-@agent_multimodal_type
-class BaseMultiModal:
-    def __init__(
-            self,
-            api_key: str,
-            model: str,
-            temperature: float,
-            safety_settings: Any = None
-        ):
-        
-        self._api_key = api_key
-        self._model = model
-        self._temperature = temperature
-        self._safety_settings = safety_settings
-        self._multi_modal = self._initialize_multimodal()
-
-
-    def _initialize_multimodal(self):
-        if self._model.startswith("gemini"): # Google models
-            return genai.GenerativeModel(
-                model_name=self._model,
-                safety_settings=self._safety_settings
-            )
-        else:
-            self._multi_modal = None
-            raise ValueError('Model not supported. currently supported models is gemini')
-
-
-    def multimodal_generate(self, prompt: str, img: Image, stream=False):
-        
-        response = self._multi_modal.generate_content([prompt, img], stream=stream)
-        response.resolve()
-
-        return response.text
-
-
-    def __str__(self) -> str:
-    
-        multimodal_initialized = self._multi_modal is not None
-
-        return f"Model used: {self.model}, wth temperature: {self._temperature}, multimodal initialized: {multimodal_initialized}"
-
-
-    def __str__(self) -> str:
-    
-        multimodal_initialized = self._multi_modal is not None
-
-        return f"Model used: {self.model}, wth temperature: {self._temperature}, multimodal initialized: {multimodal_initialized}"
-
-
-    @property
-    def multimodal(self) -> Any:
-        
-        return self._multi_modal
