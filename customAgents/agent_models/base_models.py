0c2ae2b8ce1687f746ffb45d859274f996c1394f
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-09-10 14:23:55 +0300

    defince base models types

--- customAgents/agent_models/base_models.py
+++ customAgents/agent_models/base_models.py
@@ -1,5 +1,8 @@
+from type_utils import agent_models_type
 ## This is still dummy implementation for the BaseModels.. will be updated later !!
 
+
+@agent_models_type
 class BaseModels:
     def __init__(self):
         pass
