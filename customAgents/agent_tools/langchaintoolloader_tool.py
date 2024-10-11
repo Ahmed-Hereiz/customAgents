98182e6d83788abc4f6c2b19aef5a100a904451b
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-10-11 18:33:49 +0300

    add langchaintoolloader file

---
+++ customAgents/agent_tools/langchaintoolloader_tool.py
@@ -0,0 +1,37 @@
+from langchain.tools import BaseTool
+from typing import Type, Any
+
+class LangchainToolLoader(BaseTool):
+    def __init__(self, tool_class: Type[BaseTool], *args, **kwargs):
+        self.tool = tool_class(*args, **kwargs)
+        super().__init__(
+            name=self.tool.name,
+            description=self.tool.description,
+            return_direct=self.tool.return_direct,
+            verbose=self.tool.verbose,
+            callbacks=self.tool.callbacks,
+            callback_manager=self.tool.callback_manager,
+            tags=self.tool.tags,
+            metadata=self.tool.metadata,
+        )
+
+    def _run(self, *args: Any, **kwargs: Any) -> Any:
+        return self.tool.run(*args, **kwargs)
+
+    def _arun(self, *args: Any, **kwargs: Any) -> Any:
+        return self.tool.arun(*args, **kwargs)
+
+    def execute_func(self, *args: Any, **kwargs: Any) -> Any:
+        return self._run(*args, **kwargs)
+
+# Example usage:
+# from langchain.tools import WikipediaQueryRun
+# from langchain.utilities import WikipediaAPIWrapper
+
+# wikipedia_tool = LangchainToolLoader(
+#     WikipediaQueryRun,
+#     api_wrapper=WikipediaAPIWrapper()
+# )
+# result = wikipedia_tool.execute_func("Python programming language")
+
+
