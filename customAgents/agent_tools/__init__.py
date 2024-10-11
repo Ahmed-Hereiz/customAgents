bc233148fd7bfd32fac244508843280d644961c3
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-10-12 00:28:14 +0300

    Remove GitHub scrape file

--- customAgents/agent_tools/__init__.py
+++ customAgents/agent_tools/__init__.py
@@ -2,11 +2,10 @@ from .base_tools import ToolKit, BaseTool
 from .scrapelink_tool import ScrapeLinkTool, ScrapeStaticLinkTool, ScrapeDynamicLinkTool
 from .search_tool import SearchTool
 from .readpdf_tool import PDFDocReaderTool
-from .scrapegithub_tool import GithubAccScrapeTool, GithubAccScrapeSaveTool
 from .pythonexec_tool import PythonRuntimeTool
 from .bashexec_tool import BashRuntimeTool
 from .modelinference_tool import ModelInferenceTool
-from .langchaintoolloader_tool import LangchainToolLoader
+from .langchainloader_tool import LangchainToolLoader
 
 
 __all__ = [
@@ -17,8 +16,6 @@ __all__ = [
     'ScrapeDynamicLinkTool',
     'SearchTool',
     'PDFDocReaderTool',
-    'GithubAccScrapeTool',
-    'GithubAccScrapeSaveTool',
     'PythonRuntimeTool',
     'BashRuntimeTool',
     'ModelInferenceTool',
