f570db3b65e6ae1c507270c45fa7200cde8e132f
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-10-21 16:20:48 +0300

    debug loading gradio models

--- customAgents/ml_models/gradioclient_models.py
+++ customAgents/ml_models/gradioclient_models.py
@@ -3,17 +3,20 @@ from gradio_client import Client
 
 
 class GradioClientModels(BaseModels):
-    def __init__(self, gradio_client_id : str, api_name : str = None):
-        
+    def __init__(self, gradio_client_id: str, api_name: str = None):
         self.gradio_client_id = gradio_client_id
         self.api_name = api_name
-
+        self.client = None
         super().__init__()
 
-    def inference(self, input_prompt):
+    def load_model(self):
+        self.client = Client(self.gradio_client_id)
 
-        client = Client(self.gradio_client_id)
-        client_output = client.predict(
+    def inference(self, input_prompt):
+        if self.client is None:
+            self.load_model()
+
+        client_output = self.client.predict(
             input_prompt,
             api_name=self.api_name
         )
