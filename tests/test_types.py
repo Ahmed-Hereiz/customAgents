318fa6b79d4460a5dfa7c53d1ddc50573d1e0554
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2024-11-26 19:54:35 +0200

    update test types

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,5 +1,5 @@
 from customAgents.agent_llm import BaseLLM, SimpleInvokeLLM, SimpleStreamLLM
-from customAgents.agent_prompt import BasePrompt, PlaceHoldersPrompt, SimplePrompt, ReActPrompt
+from customAgents.agent_prompt import BasePrompt, SimplePrompt, ReActPrompt
 
 llm1 = BaseLLM(api_key='dummy',model='gemini-pro',temperature=0.7)
 llm2 = SimpleInvokeLLM(api_key='dummy',model='gemini-pro',temperature=0.7)
@@ -9,10 +9,8 @@ print(type(llm2))
 print(type(llm3))
 
 prompt1 = BasePrompt()
-prompt2 = PlaceHoldersPrompt()
 prompt3 = SimplePrompt()
 prompt4 = ReActPrompt(question="dummy")
 print(type(prompt1))
-print(type(prompt2))
 print(type(prompt3))
 print(type(prompt4))
