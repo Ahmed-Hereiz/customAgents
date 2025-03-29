5e40622447fd643af7d158bcaa026a3c6ee26895
Ahmed Hereiz <ahmed.hany.fathi@gmail.com>
2025-03-29 19:37:27 +0200

    start the base agent

--- customAgents/Agents/base_agent.py
+++ customAgents/Agents/base_agent.py
@@ -1,3 +1,8 @@
 class BaseAgent:
+    "base class which will work as faster, easier runtime than normal runtime class"
     def __init__(self) -> None:
+        pass
+
+    def loop(self):
+        "base method to make the agent generate"
         pass
